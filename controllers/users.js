6bc5be3255cb3bad734e62c31db38ac02d159794
elissanyberg <esnyberg08@gmail.com>
2025-07-01 11:05:20 -0400

    User Controllers

---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+const User = require("../models/user");
+
+//GET /users
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message });
+      //something similar to this ^ in all catch blocks.
+      // will have to change the hard code od 500 at some point//
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+  User.create({ name, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const getUser = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .orFail()
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFound") {
+        return res.status(404).send({ message: err.message });
+      } else if (err.name === "CastError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { getUsers, createUser, getUser };
