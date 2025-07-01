a2c3e7388ab12049e5ccf357b216602c5173f052
elissanyberg <esnyberg08@gmail.com>
2025-07-01 13:00:17 -0400

    Error Handling

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,31 @@
 const User = require("../models/user");
-
-//GET /users
+const errors = require("../utils/errors");
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send(users))
+    .then((users) => res.status(errors.OK_STATUS_CODE).send(users))
     .catch((err) => {
       console.error(err);
-      return res.status(500).send({ message: err.message });
-      //something similar to this ^ in all catch blocks.
-      // will have to change the hard code od 500 at some point//
+      return res
+        .status(errors.INTERNAL_SERVER_ERROR_STATUS_CODE)
+        .send({ message: err.message });
     });
 };
 
 const createUser = (req, res) => {
   const { name, avatar } = req.body;
   User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
+    .then((user) => res.status(errors.CREATED_STATUS_CODE).send(user))
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
-        return res.status(400).send({ message: err.message });
+        return res
+          .status(errors.BAD_REQUEST_STATUS_CODE)
+          .send({ message: err.message });
       }
-      return res.status(500).send({ message: err.message });
+      return res
+        .status(errors.INTERNAL_SERVER_ERROR_STATUS_CODE)
+        .send({ message: err.message });
     });
 };
 
@@ -30,15 +33,21 @@ const getUser = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
     .orFail()
-    .then((user) => res.status(200).send(user))
+    .then((user) => res.status(errors.OK_STATUS_CODE).send(user))
     .catch((err) => {
       console.error(err);
       if (err.name === "DocumentNotFound") {
-        return res.status(404).send({ message: err.message });
+        return res
+          .status(errors.NOT_FOUND_STATUS_CODE)
+          .send({ message: err.message });
       } else if (err.name === "CastError") {
-        return res.status(400).send({ message: err.message });
+        return res
+          .status(errors.BAD_REQUEST_STATUS_CODE)
+          .send({ message: err.message });
       }
-      return res.status(500).send({ message: err.message });
+      return res
+        .status(errors.INTERNAL_SERVER_ERROR_STATUS_CODE)
+        .send({ message: err.message });
     });
 };
 
