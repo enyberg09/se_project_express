186f01c7c97c5780b7b57a3461abefbe5d987a09
elissanyberg <esnyberg08@gmail.com>
2025-07-09 11:02:46 -0400

    Temp ID added

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
+
 app.post("/signup", createUser);
 
 app.post("/signin", loginUser);
