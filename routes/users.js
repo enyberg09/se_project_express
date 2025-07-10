d0e2a8596003999df0e5b59744043561e87b8136
elissanyberg <esnyberg08@gmail.com>
2025-07-10 07:44:40 -0400

    Routes users.js fixes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const auth = require("../middleware/auth");
 const { getCurrentUser, updateUser } = require("../controllers/users");
 
-router.get("/users/me", auth, getCurrentUser);
+router.get("/me", auth, getCurrentUser);
 
-router.patch("/users/me", auth, updateUser);
+router.patch("/me", auth, updateUser);
 
 module.exports = router;
