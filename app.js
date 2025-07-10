9f592464c145cf57045628a2e5c8cd33104c73c2
elissanyberg <esnyberg08@gmail.com>
2025-07-10 07:45:28 -0400

    app.js fix

--- app.js
+++ app.js
@@ -18,13 +18,6 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "5d8b8592978f8bd833ca8133",
-  };
-  next();
-});
-
 app.post("/signup", createUser);
 
 app.post("/signin", loginUser);
